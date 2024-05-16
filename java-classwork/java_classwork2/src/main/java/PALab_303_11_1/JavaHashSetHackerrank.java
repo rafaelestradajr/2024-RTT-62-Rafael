fd13f62a5d6ac8c9193274a14ccb3236ab577eb8
Rafael Estrada <rafaelestrada@comcast.net>
2024-05-15 22:04:22 -0700

    Homework for Thurs 5/16

---
+++ java-classwork/java_classwork2/src/main/java/PALab_303_11_1/JavaHashSetHackerrank.java
@@ -0,0 +1,26 @@
+package PALab_303_11_1;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaHashSetHackerrank
+{
+
+    public static void main(String[] args) {
+
+        HashSet  hs = new HashSet ();
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        int numCases = Integer.parseInt(sc.nextLine());
+        for(int i = 0;i < numCases;i++){
+            if(hs.add(sc.nextLine())){
+                count++;
+            }
+            System.out.println(count);
+        }
+
+    }
+}
